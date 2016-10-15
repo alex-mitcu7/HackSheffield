ceec843960d782d621120db8f6a1ecc8fafc3bfd
Alex Mitcu <alexmitcu@gmail.com>
2016-10-15 17:19:21 +0100

    Implemented vocal function - the answer is now vocal

--- showResponse.php
+++ showResponse.php
@@ -1,8 +1,27 @@
 <?php
   session_start();
-
+  require_once('voicerss_tts.php');
   // Print the response
-  echo $_SESSION['answer'];
+
+  $string = $_SESSION['answer'];
+
+
+  $vocal = new VoiceRSS;
+  $voice = $vocal->speech([
+    'key' => 'f97c77ad9ca2437cb98a89076a952aa7',
+    'hl' => 'en-us',
+    'src' => $string,
+    'r' => '0',
+    'c' => 'wav',
+    'f' => '44khz_16bit_stereo',
+    'ssml' => 'false',
+    'b64' => 'true'
+  ]);
+
+  echo '<audio src="' . $voice['response'] . '" autoplay="autoplay"></audio>';
+
+  // Alternatively, print the content as well
+  echo $string;
 
   session_destroy();
 

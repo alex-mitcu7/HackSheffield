8b8154bbc7bb526e1b78955c59c4bb9c6a14151f
Alex Mitcu <alexmitcu@gmail.com>
2016-10-15 23:51:02 +0100

    Added animations

--- showResponse.php
+++ showResponse.php
@@ -3,8 +3,9 @@
   require_once('voicerss_tts.php');
   // Print the response
 
-  $string = $_SESSION['answer'];
 
+  //$string = $_SESSION['answer'];
+  $string = "This is just a test.";
 
   $vocal = new VoiceRSS;
   $voice = $vocal->speech([
@@ -18,8 +19,6 @@
     'b64' => 'true'
   ]);
 
-  echo '<audio src="' . $voice['response'] . '" autoplay="autoplay"></audio>';
-
 
   session_destroy();
 
@@ -29,24 +28,59 @@
  <html >
    <head>
      <meta charset="UTF-8">
-     <title>Glowing Pulse Form</title>
+     <title>Just Ask It</title>
      <link rel="stylesheet" href="./css/normalize.css">
      <link rel="stylesheet" href="./css/style.css">
+       <link rel="stylesheet" href="./css/styleAnimation.css">
      <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+     <script type="text/javascript">
+       $(window).load(function() {
+    	    $(".loader").fadeOut("slow");
+        })
+      </script>
+      <style>
+      div.inline {
+        margin-top: 5%;
+      }
+      </style>
    </head>
 
    <body>
      <section class="webdesigntuts-workshop">
+       <br>
+       <h1>Here is your answer.</h1>
+       <br>
+       <div id="ajaxloader3" class="inline">
+         <div class="outer"></div>
+         <div class="inner"></div>
+       </div>
+
+       <div id="ajaxloader3" class="inline">
+         <div class="outer"></div>
+         <div class="inner"></div>
+       </div>
+
+       <div id="ajaxloader3" class="inline">
+         <div class="outer"></div>
+         <div class="inner"></div>
+       </div>
+
        <div id = "responseDIV">
          <h2><?php echo $string; ?></h2>
+
          <form action="receiveQuestion.php" method="post">
   		       <input type="search" placeholder="What are you looking for?"
                     name="question-bar">
              <button type="submit" value="Ask me again?">Ask me again?</button>
 
   	     </form>
+         <br>
 
        </div>
+       <audio id="sound" src="<?php echo $voice['response']; ?>" preload="auto"></audio>
+       <button id ="volume" onclick="document.getElementById('sound').play();">
+         Play it
+       </button>
 
      </section>
 
